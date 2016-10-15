d1e1e8150ca08213298bbbbc5813de1b522b18c1
Matthew Chin <Matthew Chin>
2016-10-15 10:28:48 +0100

    Lab 6
    Implemented Go Back N into client/server
    
    There is a bug that I believe is related to how ACKs are tracked in the client

--- UDPEchoClient.java
+++ UDPEchoClient.java
@@ -1,6 +1,7 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 
 /**
  * UDPEchoClient Class reads in a file, splits it into n datagrams each of 20
@@ -21,10 +22,10 @@ public class UDPEchoClient {
         int temp = 0;
         int fileSize = 0;
 
-        int snMax = 0;
-        int snMin = 0;
-        int sn = 0;
-        int rn = 0;
+        //go back n vars
+        int sendPackNum = 0;
+        int winSize = 0;
+        int pcktNum = 0;
 
         boolean done = false;
         boolean ack = false;
@@ -39,19 +40,25 @@ public class UDPEchoClient {
         //sequence number of sent UDP datagram, 0|1, alternating depending on ACK receipt
         byte seqNum = 0;
 
-        if ((args.length != 2)) {
-            System.out.println("Usage: %s <Gateway IP> [<Gateway Port>]\n");
+        if ((args.length != 3)) {
+            System.out.println("Usage: %s <Gateway IP> <Gateway Port> <Sliding Window Size>\n");
             System.exit(1);
         }
 
         gatewayIP = args[0];
+        gatewayPort = (new Integer(args[1]));
 
-        if (args.length == 2) {
-            gatewayPort = (new Integer(args[1]));
-        } else {
-            gatewayPort = 7;
-        }
+        //init the window
+        winSize = Integer.parseInt(args[2]);
 
+        /**
+         * this will hold the packets that we have sent that correspond to the
+         * window size, we then store those in here waiting for an ACK when we
+         * receive an ACK we can remove the corresponding packet from the
+         * ArrayList
+         */
+        ArrayList<DatagramPacket> winBuffer = new ArrayList<>();
+        ArrayList<DatagramPacket> remBuffer = new ArrayList<>();
         try {
             clientSocket = new DatagramSocket();
             gatewayIPAddress = InetAddress.getByName(gatewayIP);
@@ -73,56 +80,142 @@ public class UDPEchoClient {
                 }
                 inputStream.close();
 
+                //count should be 2689 for ascii.gif
                 count = (fileSize / 19) + ((fileSize % 19 == 0) ? 0 : 1);
 
                 /**
-                 * TODO: 
+                 * send out winSize packets to the server after each send,
+                 * calculate the frameNum and add it to winBuffer of size
+                 * winSize; keep sending these n packets; when we receive ACKS
+                 * we send the next p packets that haven't been worked on yet
+                 * with p being the number of packets we have ACKs for
                  */
-                //fill sendBuffer
-                for (int i = 0; i < (count - 1); i++) {
-                    for (int j = 1; j < 20; j++) {
-                        sendBuffer[j] = buffer[i * 19 + j - 1];
+                boolean flag = false;
+                int t = 0;
+                while (ackCount != (count - 1)) {
+                    for (t = 0; t < winSize; t++) {
+                        //pcktNum is replacing this for loop count
+                        if (seqNum == 0) {
+                            sendBuffer[0] = seqNum++;
+                        } else {
+                            sendBuffer[0] = seqNum--;
+                        }
+                        for (int j = 1; j < 20; j++) {
+                            //we have not gone through the first round
+                            //of the window, 0 -> winSize
+                            if (!flag) {
+                                sendBuffer[j] = buffer[t * 19 + j - 1];
+                            } else {
+                                /**
+                                 * we have gone through the first round now we
+                                 * must start accumulating iteration counts into
+                                 * pcktNum
+                                 *
+                                 * working with pcktNum the first time we need
+                                 * to modify it to winSize anytime afterwards we
+                                 * increment pcktNum which should get the
+                                 * correct packet from buffer
+                                 */
+
+                                //go back
+                                if (ackCount < pcktNum) {
+                                    pcktNum = ackCount + 1;
+                                }
+
+                                if (pcktNum == winSize) {
+                                    sendBuffer[j] = buffer[pcktNum * 19 + j - 1];
+                                    pcktNum += 1;
+                                } else {
+                                    //System.out.println("pcktNum: " + pcktNum);
+                                    sendBuffer[j] = buffer[pcktNum * 19 + j - 1];
+                                    pcktNum += 1;
+                                }
+                            }
+                        }
+
+                        /**
+                         * check if winBuffer is empty: if it is then move ahead
+                         * since this is the first time running through
+                         * otherwise this is not our first time and we have
+                         * packets that we did not receive an ACK for, so resend
+                         * them here along with any additional packets up to the
+                         * winSize
+                         */
+                        if (!(winBuffer.isEmpty())) {
+                            for (DatagramPacket dp : winBuffer) {
+                                clientSocket.send(dp);
+                            }
+                        }
+
+                        //send winSize packets over to the server
+                        sendDatagramPacket = new DatagramPacket(sendBuffer, sendBuffer.length, gatewayIPAddress, gatewayPort);
+                        clientSocket.send(sendDatagramPacket);
+
+                        //keep actual count of first set of iterations
+                        if (!flag) {
+                            pcktNum++;
+                        }
+                        /**
+                         * put the packet into the winBuffer this will grow the
+                         * window buffer with unACK'ed packets **WE NEED TO HOLD
+                         * THIS TO THE ACTUAL WINDOW SIZE!**
+                         */
+                        winBuffer.add(sendDatagramPacket);
+                    }
+                    /**
+                     * update flag if we have gone through the initial set of
+                     * winSize iterations, after the first iteration this should
+                     * never run again, because flag will always be false
+                     */
+                    if ((pcktNum == (winSize)) && !flag) {
+                        flag = true;
                     }
 
-                    //send first set of datagrams to gateway
-                    sendDatagramPacket = new DatagramPacket(sendBuffer, sendBuffer.length, gatewayIPAddress, gatewayPort);
-                    clientSocket.send(sendDatagramPacket);
-                    while (!ack) {
-                        //set socket to timeout in 1.5 seconds
-                        //after not receiving a message back from server
-                        clientSocket.setSoTimeout(50);
+                    /**
+                     * we sent winSize packets to the server, now we handle ACKs
+                     * loop through winBuffer and check if we have received ACKS
+                     * for these packets if so remove them; try this a few
+                     * times?
+                     */
+                    clientSocket.setSoTimeout(50);
+                    for (DatagramPacket c : winBuffer) {
+                        //clientSocket.setSoTimeout(50);
                         try {
-                            //try to receive a message from server
                             clientSocket.receive(rcvdDatagramPacket);
-
-                            //we got message back from server, indicating
-                            //receipt of datagram packet
-                            if (rcvdDatagramPacket.getData()[0] > snMin) {
-                                snMin = rcvdDatagramPacket.getData()[0];
-                            }
-                            /**
-                             * if snMin < snMax and no frame is in transmission
-                             * set sn bewteeen snMin and snMax transmit sn
-                             */
-                            if (snMin < snMax) {
-                                sn = snMin + 1;
-
-                                sendDatagramPacket = new DatagramPacket(sendBuffer, sendBuffer.length, gatewayIPAddress, gatewayPort);
-                                clientSocket.send(sendDatagramPacket);
+                            if (seqNum == 0) {
+                                if (rcvdDatagramPacket.getData()[0] != seqNum) {
+                                    System.out.println("Received ACK: " + ackCount);
+                                    ackCount++;
+                                    remBuffer.add(c);
+                                }
+                            } else {
+                                if (rcvdDatagramPacket.getData()[0] != seqNum) {
+                                    System.out.println("Received ACK: " + ackCount);
+                                    ackCount++;
+                                    remBuffer.add(c);
+                                }
                             }
                         } catch (SocketTimeoutException soex) {
-                            //resend the datagram again
-                            clientSocket.send(sendDatagramPacket);
                         }
                     }
-
-                    //reset received message check flag
-                    ack = false;
-                    ackCount++;
+                    /**
+                     * clear out the remove buffer of ACK'ed packets
+                     */
+                    remBuffer.clear();
+                    //we have sent all 2688 packets; break
                 }
+
+                //update the seqChk
+                if (seqNum == 0) {
+                    sendBuffer[0] = seqNum++;
+                } else {//make seqNum = 1
+                    sendBuffer[0] = seqNum--;
+                }
+
                 //clear out old send buffer
                 sendBuffer = new byte[20];
-                sendBuffer[0] = seqNum;
+                sendBuffer[0] = 0;
+
                 //put last few uneven bytes into sendBuffer
                 for (int k = (count - 1) * 19; k < fileSize; k++) {
                     sendBuffer[k - (count - 1) * 19 + 1] = buffer[k];
@@ -138,7 +231,7 @@ public class UDPEchoClient {
                         clientSocket.receive(rcvdDatagramPacket);
                         if (rcvdDatagramPacket.getData()[0] == (seqNum)) {
                             ack = true;
-                            System.out.println("Received ACK: " + (ackCount + 1));
+                            //System.out.println("Received ACK: " + (ackCount + 1));
                         }
                     } catch (SocketTimeoutException ex) {
                         clientSocket.send(sendDatagramPacket);
--- UDPEchoServer.java
+++ UDPEchoServer.java
@@ -1,7 +1,6 @@
 
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
 
 /**
  * UDPEchoServer that receives a n datagram packets from the gateway which
@@ -24,16 +23,14 @@ public class UDPEchoServer {
 
         byte seqChk = 0;
 
-        int rn = 0;
-
         int count = 0;
         int serverPort = 0;
 
         boolean done = false;
 
-        if (args.length != 1) {
+        if (args.length != 2) {
             System.out.println("args.length is " + args.length + "\n");
-            System.out.println("Usage:  %s <UDP SERVER PORT>, <UDP SERVER IP>\n");
+            System.out.println("Usage:  %s <UDP SERVER PORT> <Window Size>\n");
             System.exit(1);
         }
 
@@ -77,34 +74,39 @@ public class UDPEchoServer {
                 if (count != 2688) { //we do not have the last datagram
 
                     /**
-                     * we have received a good packet from the Client where sn =
-                     * rn, send an ACK back and rn++ for next expected frame
+                     * we found a repeat seqNum, send an ACK back to client
+                     * saying we already got this; this is needed in the event
+                     * the gateway drops our original ACK; skip to the next
+                     * iteration, not incrementing count
                      */
-                    if (pack.getData()[0] == rn) {
+                    if (pack.getData()[0] != seqChk) {
                         ack = new DatagramPacket(pack.getData(), pack.getData().length,
                                 pack.getAddress(), pack.getPort());
                         serverSocket.send(ack);
-                        rn++;
+                        continue;
                     }
+
                     /**
-                     * loop through 19 bytes excluding the seqChk, mult 19 by
-                     * the seqChk to get the last place we buffered in data then
-                     * add an offset of k to read ahead
+                     * loop through 19 bytes excluding the seqChk, multiply 19
+                     * by the seqChk to get the last place we buffered in data
+                     * then add an offset of k to read ahead
                      */
                     for (int k = 0; k < 19; k++) {
                         rcvdBuffer[count * 19 + k] = pack.getData()[k + 1];
                     }
+                    /**
+                     * here we have successfully received a datagram packet send
+                     * a datagram back to client
+                     */
+                    ack = new DatagramPacket(pack.getData(), pack.getData().length, pack.getAddress(), pack.getPort());
+                    serverSocket.send(ack);
 
-                    //here we have successfully received a datagram packet
-                    //send a datagram back to client
-//                    ack = new DatagramPacket(pack.getData(), pack.getData().length, pack.getAddress(), pack.getPort());
-//                    serverSocket.send(ack);
-                    //update seqChk for the next datagram
-//                    if (seqChk == 0) {
-//                        seqChk = 1;
-//                    } else {
-//                        seqChk = 0;
-//                    }
+                    //update seqChk for the next datagram  
+                    if (seqChk == 0) {
+                        seqChk = 1;
+                    } else {
+                        seqChk = 0;
+                    }
                 } else { //do we have the last datagram?
                     //we're at the last datagram so read from 1 + the last index until the end of the file
                     for (int n = count * 19; n < 51080; n++) {
